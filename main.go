80e417e37fa195a56bcdcf23e32447b56f1b2aa2
Kyler Johnson <kylerjohnsondev@gmail.com>
2021-11-03 21:53:56 -0400

    add subtraction operation

--- main.go
+++ main.go
@@ -8,30 +8,30 @@ import (
 	"strings"
 )
 
-func main () {
+func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Calculator is up and running \n Press 'enter' to continue...")
 	fmt.Scanln()
 	var operation_selection string
-	
-	for(strings.Trim(operation_selection, "\r\n") != "exit") {
+
+	for strings.Trim(operation_selection, "\r\n") != "exit" {
 		fmt.Println("What would you like to do?\n1) Add\n2) Subtract\n3) Multiply\n4) Divide\nOr type \"exit\" to stop.")
-		operation_selection, _ = reader.ReadString('\n') 
+		operation_selection, _ = reader.ReadString('\n')
 		trimmed_op_selection := strings.Trim(operation_selection, "\r\n") // why \r\n?
-	
+
 		switch trimmed_op_selection {
-			case "1":
-				operations.Start_addition_operation(*reader)
-			case "2":
-				fmt.Println(fmt.Sprintf("You selected %v", trimmed_op_selection))
-			case "3":
-				fmt.Println(fmt.Sprintf("You selected %v", trimmed_op_selection))
-			case "4":
-				fmt.Println(fmt.Sprintf("You selected %v", trimmed_op_selection))
-			case "exit":
-				fmt.Println(fmt.Sprintf("You selected %v", trimmed_op_selection))
-			default: 
-				fmt.Println("Not a valid selection. Please select an operation from the menu.")
-		} 
+		case "1":
+			operations.Start_addition_operation(*reader)
+		case "2":
+			operations.Start_subtraction_operation(*reader)
+		case "3":
+			fmt.Println(fmt.Sprintf("You selected %v", trimmed_op_selection))
+		case "4":
+			fmt.Println(fmt.Sprintf("You selected %v", trimmed_op_selection))
+		case "exit":
+			fmt.Println(fmt.Sprintf("You selected %v", trimmed_op_selection))
+		default:
+			fmt.Println("Not a valid selection. Please select an operation from the menu.")
+		}
 	}
 }
--- operations/operations.go
+++ operations/operations.go
@@ -7,6 +7,22 @@ import (
 	"strings"
 )
 
+func parse_number_inputs(number_input string) []int64 {
+	trimmed_input := strings.Trim(number_input, "\r\n")
+	pre_parsed_nums := strings.Split(trimmed_input, ",")
+	var nums []int64
+	for _, num_string := range pre_parsed_nums {
+		trimmed := strings.Trim(num_string, " ")
+		number, err := strconv.ParseInt(trimmed, 10, 64)
+		if err != nil {
+			fmt.Println(fmt.Sprintf("Unable to parse string input into numbers. Input: %v", number_input))
+			fmt.Println(err)
+		}
+		nums = append(nums, number)
+	}
+	return nums
+}
+
 func add_numbers(numbers []int64) int64 {
 	var sum int64 = 0
 	for _, num := range numbers {
@@ -18,20 +34,29 @@ func add_numbers(numbers []int64) int64 {
 func Start_addition_operation(reader bufio.Reader) {
 	fmt.Println("Please type all of the numbers you wish to add, separated by a comma below.")
 	nums_to_add_input, _ := reader.ReadString('\n')
-	trimmed_input := strings.Trim(nums_to_add_input, "\r\n")
-	nums_to_add_text := strings.Split(trimmed_input, ",")
-	var nums_to_add []int64
-	for _, num_string:= range nums_to_add_text {
-		trimmed := strings.Trim(num_string, " ")
-		number, err := strconv.ParseInt(trimmed, 10, 64)
-		if err != nil {
-			fmt.Println(fmt.Sprintf("Unable to parse string input into numbers. Input: %v", nums_to_add_input))
-			fmt.Println(err)
-			return
-		}
-		nums_to_add = append(nums_to_add, number)
-	}
+	nums_to_add := parse_number_inputs(nums_to_add_input)
 	sum := add_numbers(nums_to_add)
 	fmt.Println(fmt.Sprintf("Sum: %v", sum))
+	fmt.Scanln()
+}
+
+func subtract_numbers(numbers []int64) int64 {
+	var difference int64 = 0
+	for index, num := range numbers {
+		if index == 0 {
+			difference = num
+		} else {
+			difference = difference - num
+		}
+	}
+	return difference
+}
+
+func Start_subtraction_operation(reader bufio.Reader) {
+	fmt.Println("Please type all of the numbers you wish to subtract in order, separated by a comma below.")
+	nums_to_subtract_input, _ := reader.ReadString('\n')
+	nums_to_subtract := parse_number_inputs(nums_to_subtract_input)
+	difference := subtract_numbers(nums_to_subtract)
+	fmt.Println(fmt.Sprintf("Difference: %v", difference))
 	fmt.Scanln()
 }
